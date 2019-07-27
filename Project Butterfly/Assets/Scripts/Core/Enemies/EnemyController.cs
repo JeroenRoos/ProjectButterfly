1f3b301b685811f0783947dda60e693927ed5ed9
Jeroen Roos <j.roos@arlanet.com>
2019-07-27 15:01:50 +0200

    Seperated enemy and coin spawner

--- Project Butterfly/Assets/Scripts/Core/Enemies/EnemyController.cs
+++ Project Butterfly/Assets/Scripts/Core/Enemies/EnemyController.cs
@@ -6,34 +6,39 @@ public class EnemyController : MonoBehaviour
     public GameObject player;
     public GameObject[] sPrefabs;
     public GameObject[] enemy;
-    private Vector3 spawnObstaclePosition;
+    private Vector3 spawnObstaclePositionEnemy;
+    private Vector3 spawnObstaclePositionCoin;
 
     void Update()
     {
-        float distanceToHorizon = Vector2.Distance(player.gameObject.transform.position, spawnObstaclePosition);
+        float distanceToHorizonEnemy = Vector2.Distance(player.gameObject.transform.position, spawnObstaclePositionEnemy);
+        float distanceToHorizonCoin = Vector2.Distance(player.gameObject.transform.position, spawnObstaclePositionCoin);
 
         //hoger getal en ze worden verderweg al gerenderd
-        if (distanceToHorizon < 10)
+        if (distanceToHorizonEnemy < 10)
+        {
+            SpawnEnemy();
+        }
+
+        if (distanceToHorizonEnemy < 10)
         {
             SpawnCoins();
-
-            SpawnEnemy();
         }
     }
 
     private void SpawnCoins()
     {
-        spawnObstaclePosition = new Vector3(spawnObstaclePosition.x + Random.Range(0.1f, 10f), Random.Range(-0.5f, 0.7f), 0);
+        spawnObstaclePositionCoin = new Vector3(spawnObstaclePositionCoin.x + Random.Range(0.1f, 10f), Random.Range(-0.5f, 0.7f), 0);
 
         //Quaternion zorgt voor de ingestelde draai en alles
-        Instantiate(sPrefabs[(Random.Range(0, sPrefabs.Length))], spawnObstaclePosition, Quaternion.identity); 
+        Instantiate(sPrefabs[(Random.Range(0, sPrefabs.Length))], spawnObstaclePositionCoin, Quaternion.identity); 
     }
 
     private void SpawnEnemy()
     {
-        spawnObstaclePosition = new Vector3(spawnObstaclePosition.x + Random.Range(0.1f, 5f), -0.8f, 0);
+        spawnObstaclePositionEnemy = new Vector3(spawnObstaclePositionEnemy.x + Random.Range(0.1f, 5f), -0.8f, 0);
 
         //Quaternion zorgt voor de ingestelde draai en alles
-        Instantiate(enemy[(Random.Range(0, enemy.Length))], spawnObstaclePosition, Quaternion.identity); 
+        Instantiate(enemy[(Random.Range(0, enemy.Length))], spawnObstaclePositionEnemy, Quaternion.identity); 
     }
 }
